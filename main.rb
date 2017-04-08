71095d944409cc48ef94faa6f5b4d8103a4941d3
Mr.Pain <ramil.talipov@gmail.com>
2017-04-08 22:38:39 +0300

    version 2

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require "digest"
+
 if Gem.win_platform?
   Encoding.default_external = Encoding.find(Encoding.locale_charmap)
   Encoding.default_internal = __ENCODING__
@@ -13,3 +15,9 @@ phrase = STDIN.gets.encode("UTF-8").chomp
 
 puts "Каким образом зашифровать:"
 ['MD5', 'SHA1'].each_with_index { |item, index| puts "#{index + 1}. #{item}" }
+choice = STDIN.gets.chomp
+
+case
+  when 1 then puts Digest::MD5.hexdigest(phrase)
+  when 2 then puts Digest::SHA1.hexdigest(phrase)
+end
